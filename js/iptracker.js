5c8703dbaee32cee44eb241f2af1c335075f011a
z3ox1s <66672234+z3ox1s@users.noreply.github.com>
2021-01-30 18:29:35 -0300

    Main script of the website

--- js/iptracker.js
+++ js/iptracker.js
@@ -1,36 +1,18 @@
+// Listener that makes the call of 'load' function
 document.querySelector('body').addEventListener('load', load())
 
+// Main function when the page is loaded
 function load() {
-    fetch(`https://ipwhois.app/json/`)
-        .then (function (response) {
-            if (!response.ok) {
-                alert('Invalid/Nonexistant IP!')
-            }
-
-            return response.json()
-        })
-        
-        .then (function (data) {
-            if (data.message == "invalid IP address") {
-                alert('Invalid/Nonexistant IP!')
-            } else {
-                document.getElementById('iptracked').innerHTML = `<strong>IP Number: </strong>${(data.ip)}`
-                document.getElementById('continent').innerHTML = `<strong>Continent: </strong>${(data.continent)}`
-                document.getElementById('country').innerHTML = `<strong>Country: </strong>${(data.country)}`
-                document.getElementById('state').innerHTML = `<strong>Region: </strong>${(data.region)}`
-                document.getElementById('city').innerHTML = `<strong>City: </strong>${(data.city)}`
-                document.getElementById('lat').innerHTML = `<strong>Latitude: </strong>${(data.latitude)}`
-                document.getElementById('lon').innerHTML = `<strong>Longitude: </strong>${(data.longitude)}`
-                document.getElementById('timezone').innerHTML = `<strong>Timezone: </strong>${(data.timezone)}`
-                document.getElementById('provider').innerHTML = `<strong>Provider: </strong>${(data.isp)}` 
-            }
-        })
+    if (String(document.getElementById('ip').value) === '') {
+        locate(`https://ipwhois.app/json/`)
+    }
 }
 
-function locate() {
-    var ip = String(document.getElementById('ip').value)
+// Function that makes the request
+function locate(address) {
+    var address = `https://ipwhois.app/json/${String(document.getElementById('ip').value)}`
 
-    fetch(`https://ipwhois.app/json/${ip}`)
+    fetch(address)
         .then (function (response) {
             if (!response.ok) {
                 alert('Invalid/Nonexistant IP!')
@@ -40,7 +22,7 @@ function locate() {
         })
 
         .then (function (data) {
-            if (data.message == "invalid IP address") {
+            if (data.message == "Invalid IP address") {
                 alert('Invalid/Nonexistant IP!')
             } else {
                 document.getElementById('iptracked').innerHTML = `<strong>IP Number: </strong>${(data.ip)}`
